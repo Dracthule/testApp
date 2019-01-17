1445a98f61db073af4f5b5a57290629ce9728ac7
Dracthule <43589285+Dracthule@users.noreply.github.com>
2019-01-17 02:18:19 -0800

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  // tslint:disable-next-line:component-selector
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HomeComponent {
+  //  array: [
+  //   {
+  //     title: 'Sexy Vannah',
+  //     subtitle: 'Vannah is LIVE!',
+  //     description:
+  //       'Vannah is one of the worlds top models with millions of fans accross the nation.',
+  //     src: 'https://static.videogular.com/assets/videos/videogular.mp4',
+  //     type: 'video/mp4'
+  //   }
+  // ];
 }
